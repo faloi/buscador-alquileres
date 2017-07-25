414f15df80802353faf5e65414e6717e98ac2589
Federico Aloi <federico.aloi@gmail.com>
2017-07-25 16:41:52 -0300

    Notificador de Telegram

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+require 'json'
 require 'scraperwiki'
 require 'mechanize'
 require 'ostruct'
@@ -35,5 +38,34 @@ class MorphNotifier
   end
 end
 
+class TelegramNotifier
+  def initialize(ifttt_key)
+    @ifttt_key = ifttt_key
+  end
+
+  def notify!(resultado)
+    id_viejos = ScraperWiki.select('id from data').map { |x| x['id']}
+    resultado
+      .reject { |aviso| id_viejos.include? aviso[:id] }
+      .each { |aviso| send_message! aviso }
+  end
+
+  def send_message!(aviso)
+    post_json! "https://maker.ifttt.com/trigger/zonaprop_scraper/with/key/#{@ifttt_key}", {value1: "#{aviso[:titulo]} (#{aviso[:precio]})", value2: aviso[:direccion], value3: aviso[:url]}
+  end
+
+  def post_json!(url, data)
+    uri = URI.parse(url)
+
+    request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+    request.body = data.to_json
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    http.request(request)
+  end
+end
+
 resultado = ZonaPropWebScraper.new.leer_avisos!
-[MorphNotifier.new].each { |n| n.notify! resultado }
+[TelegramNotifier.new(ENV['MORPH_IFTTT_KEY']), MorphNotifier.new].each { |n| n.notify! resultado }

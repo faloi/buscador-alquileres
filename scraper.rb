490c5d9fde69c498208916453cce6d0200ab6920
Federico Aloi <federico.aloi@gmail.com>
2017-07-25 15:53:36 -0300

    Scraper terminado

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,30 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
+require 'ostruct'
+require 'active_support/all'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+class ZonaPropWebScraper
+  def initialize
+    @agent = Mechanize.new
+  end
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+  def leer_avisos!
+    leer_avisos_con_tipo!('ph') + leer_avisos_con_tipo!('casas')
+  end
+
+  def leer_avisos_con_tipo!(tipo)
+    page = @agent.get "http://www.zonaprop.com.ar/#{tipo}-alquiler-almagro-boedo-caballito-flores-parque-chacabuco-villa-crespo-4-ambientes.html"
+
+    page.search('li.post').map do |aviso|
+      {
+        :id => aviso['id'].gsub('aviso-', ''),
+        :tipo => tipo,
+        :titulo => aviso.search('.post-titulo').text,
+        :barrio => aviso.search('.post-location span').text.gsub(', Capital Federal', ''),
+        :direccion => aviso.search('.post-location').text.gsub(/\n|\t/, ''),
+        :precio => aviso.search('.precio-valor').text.strip,
+        :url => 'http://www.zonaprop.com.ar' + aviso.search('.post-titulo a').attr('href').value
+      }
+    end
+  end
+end

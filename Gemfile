6410985d7f2d6992b8f1bf09ed5f2bea5a459775
Federico Aloi <federico.aloi@gmail.com>
2020-07-27 23:44:51 -0300

    Tests de verdad con VCR

--- Gemfile
+++ Gemfile
@@ -2,15 +2,17 @@
 # specified here will be installed and made available to your morph.io scraper.
 # Find out more: https://morph.io/documentation/ruby
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-ruby "2.6.3"
+ruby '2.6.3'
 
-gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
-gem "mechanize"
-gem "pry"
-gem "activesupport"
+gem 'activesupport'
+gem 'mechanize'
+gem 'pry'
+gem 'scraperwiki', git: 'https://github.com/openaustralia/scraperwiki-ruby.git', branch: 'morph_defaults'
 
 group :test do
-  gem "rspec"
+  gem 'rspec'
+  gem 'vcr'
+  gem 'webmock'
 end
--- spec/scrapers_spec.rb
+++ spec/scrapers_spec.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require_relative '../lib/scrapers'
 
-# describe ArgenpropScraper do
-#   describe '#leer_avisos!' do
-#     let(:avisos) { ArgenpropScraper.leer_avisos! }
+describe ArgenpropScraper do
+  describe '#leer_avisos!', :vcr do
+    let(:avisos) { ArgenpropScraper.leer_avisos! }
 
-#     it { expect(avisos).to eq [] }
-#   end
-# end
+    it { expect(avisos[0]).to eq({ id: '7930909', tipo: 'ph', titulo: 'PH TIPO CASA :: 2 HABITACIONES :: COCHERA :: PATIO :: UNICO', direccion: 'Bouchard 3700', precio: '$ 25.000', url: 'https://www.argenprop.com/ph-en-alquiler-en-caseros-3-ambientes--7930909' }) }
+  end
+end
 
 describe ArgencasasScraper do
-  describe '#leer_avisos!' do
+  describe '#leer_avisos!', :vcr do
     let(:avisos) { ArgencasasScraper.leer_avisos! }
 
-    it { expect(avisos).to eq [] }
+    it { expect(avisos[0]).to eq({ id: '154237', tipo: 'casas', titulo: 'En zona com/res de Moreno Sur GBA Zona Oeste alquiler de casa de estilo clasico', direccion: 'Hipólito Yrigoyen al 0 - Moreno Sur GBA Zona Oeste', precio: '$ 26.000', url: 'https://www.argencasas.com/propiedad-casa-alquiler-moreno-sur-154-237' }) }
   end
 end
